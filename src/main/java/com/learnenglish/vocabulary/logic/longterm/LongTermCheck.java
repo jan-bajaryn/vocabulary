89c94228b1d7cbb9f093d25ec2585fd7fc800826
Jan <jabajaryn@gmail.com>
2019-09-07 16:57:13 +0300

    hp128.txt

--- src/main/java/com/learnenglish/vocabulary/logic/longterm/LongTermCheck.java
+++ src/main/java/com/learnenglish/vocabulary/logic/longterm/LongTermCheck.java
@@ -14,7 +14,6 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import static com.learnenglish.vocabulary.logic.pack1.LowTermCheck.PREFIX_FOLDER;
